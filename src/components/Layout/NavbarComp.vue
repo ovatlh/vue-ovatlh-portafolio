44182a2e1b69e9d709ac73aad1d96e287158caac
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2021-03-17 00:49:05 -0600

    NavbarComp
    - part 1

--- src/components/Layout/NavbarComp.vue
+++ src/components/Layout/NavbarComp.vue
@@ -5,6 +5,7 @@
     <a href="#" @click.prevent="mthGoArea('servicios')">Servicios</a>
     <a href="#" @click.prevent="mthGoArea('proyectosrecientes')">Proyectos</a>
     <a href="#" @click.prevent="mthGoArea('tecnologias')">Tecnologias</a>
+    <img class="menu" :src="imgClose" onload="SVGInject(this)">
   </div>
 </template>
 
@@ -14,7 +15,10 @@ export default {
   props: [],
   mounted() {},
   data() {
-    return {};
+    return {
+      imgMenu: require("@/assets/icons/menu.svg"),
+      imgClose: require("@/assets/icons/close-button.svg"),
+    };
   },
   methods: {
     mthGoArea(areaID) {
@@ -32,22 +36,45 @@ export default {
   grid-area: AppVue;
 
   display: grid;
-  grid-auto-flow: column;
-  grid-template-rows: 1fr;
+  grid-auto-flow: var(--navbar-grd-flow);
+  grid-template-columns: var(--navbar-grd-columns);
+  grid-template-rows: var(--navbar-grd-rows);
   justify-content: start;
   position: sticky;
-  width: calc(100%);
-  left: 0;
-  top: 0;
-  align-self: start;
+  width: var(--navbar-width);
+  top: var(--navbar-top);
+  bottom: var(--navbar-bottom);
+  left: var(--navbar-left);
+  right: var(--navbar-right);
+  justify-self: var(--navbar-justify-self);
+  align-self: var(--navbar-align-self);
+  text-align: end;
   background-color: rgba(0, 0, 0, 0.2);
 }
 
 #navbar a {
   text-decoration: none;
   color: #ffffff;
-  padding: 10px 5px;
+  padding: var(--navbar-a-pdd);
   font-weight: 600;
   font-size: 14px;
+}
+
+.menu {
+  display: var(--navbar-menu-display);
+  justify-content: center;
+  align-content: center;
+  align-self: center;
+  justify-self: end;
+
+  min-width: 40px;
+  max-width: 40px;
+  min-height: 40px;
+  max-height: 40px;
+
+  fill: #1e1e1e;
+  background-color: #f5f5f5;
+  padding: 10px;
+  border-radius: 10px;
 }
 </style>
